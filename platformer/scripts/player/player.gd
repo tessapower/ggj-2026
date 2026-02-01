867cb956241728687e74e346ea80b0623bccf07d
KaboSD <signkabob@gmail.com>
2026-02-01 11:10:00 -0600

    Whoops forgot to commit. Now can respawning.

--- platformer/scripts/player/player.gd
+++ platformer/scripts/player/player.gd
@@ -1,5 +1,12 @@
 class_name Player extends CharacterBody2D
 
+var respawnpoint = Vector2i(0,0)
+
+## Whether the player can double jump
 var can_double_jump := true
+
 var has_double_jumped := false
-#https://github.com/tessapower/ggj-2026
+
+## change the respawn point
+func change_respawnpoint(new_coords):
+	respawnpoint = new_coords
