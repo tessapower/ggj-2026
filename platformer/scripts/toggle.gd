f38d374e136554b4c23f970d801b92b2d087c04b
Tessa Power <hello@tessapower.xyz>
2026-02-01 02:17:16 -0600

    Support using color group functions to toggle behavior

--- platformer/scripts/toggle.gd
+++ platformer/scripts/toggle.gd
@@ -1,22 +1,28 @@
+# toggle.gd
+
 extends TileMapLayer
 
-## Physics layers: 2 = Red, 3 = Blue, 4 = Green
-@export var layer_value: int = -1
+var is_activated: bool = false
+const TILE_OFFSET: int = 4
 
-func _ready() -> void:
-	if layer_value == -1:
-		printerr("Layer not initialized!!")
 
 func mask_color_activate() -> void:
-	var cells = get_used_cells();
-	for i in cells.size():
-		var coords = cells[i]
+	if is_activated:
+		return # Already activated, do nothing
+
+	is_activated = true
+	var cells = get_used_cells()
+	for coords in cells:
 		var atlas_coords = get_cell_atlas_coords(coords)
-		set_cell(Vector2i(coords.x,coords.y), layer_value, Vector2i(atlas_coords.x, atlas_coords.y + 4));
+		set_cell(coords, 0, Vector2i(atlas_coords.x, atlas_coords.y + TILE_OFFSET))
+
 
 func mask_color_deactivate() -> void:
-	var cells = get_used_cells();
-	for i in cells.size():
-		var coords = cells[i]
+	if not is_activated:
+		return # Already deactivated, do nothing
+
+	is_activated = false
+	var cells = get_used_cells()
+	for coords in cells:
 		var atlas_coords = get_cell_atlas_coords(coords)
-		set_cell(Vector2i(coords.x,coords.y), layer_value, Vector2i(atlas_coords.x, atlas_coords.y - 4));
+		set_cell(coords, 0, Vector2i(atlas_coords.x, atlas_coords.y - TILE_OFFSET))
