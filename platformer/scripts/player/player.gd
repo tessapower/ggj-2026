60fe2af11ecb88454e70a4dd715cc92357a8cc56
KaboSD <signkabob@gmail.com>
2026-02-01 12:47:45 -0600

    Completed goal scene and script

--- platformer/scripts/player/player.gd
+++ platformer/scripts/player/player.gd
@@ -1,6 +1,7 @@
 class_name Player extends CharacterBody2D
 
-var respawnpoint = Vector2i(0,0)
+# Initial respawn point is (0,0).  
+var respawnpoint = Vector2i(global_position)
 
 ## Whether the player can double jump
 var can_double_jump := true
