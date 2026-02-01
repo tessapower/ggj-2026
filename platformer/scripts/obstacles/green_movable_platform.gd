a9eb13728058446729aa814b36376acf03b8b7ad
David Hayes <drhayes@omg.lol>
2026-01-31 21:47:10 -0600

    The green movable platform.

---
+++ platformer/scripts/obstacles/green_movable_platform.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var layer_value: int = 0
+
+@onready var animatable_body_2d: AnimatableBody2D = $AnimatableBody2D
+@onready var activated_sprite: Sprite2D = $AnimatableBody2D/ActivatedSprite
+@onready var deactivated_sprite: Sprite2D = $AnimatableBody2D/DeactivatedSprite
+
+
+func mask_color_activate() -> void:
+	activated_sprite.visible = true
+	deactivated_sprite.visible = false
+	animatable_body_2d.set_collision_layer_value(layer_value, true)
+
+
+func mask_color_deactivate() -> void:
+	activated_sprite.visible = false
+	deactivated_sprite.visible = true
+	animatable_body_2d.set_collision_layer_value(layer_value, false)
