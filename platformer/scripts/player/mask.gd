98a14edda919f0d64bd856f0722ac60383434f0d
Tessa Power <hello@tessapower.xyz>
2026-02-01 16:18:09 -0600

    Move mask enabling logic from game to MaskManager

--- platformer/scripts/player/mask.gd
+++ platformer/scripts/player/mask.gd
@@ -4,9 +4,8 @@ extends Node
 
 
 func _on_mask_pickup(body: Node2D) -> void:
-	print(body.name)
 	if body.name == "Player":
-		Game.enable_mask_color(mask_color)
+		MaskManager.enable_mask_color(mask_color)
 		
 		# Remove mask node from the scene
 		queue_free()
