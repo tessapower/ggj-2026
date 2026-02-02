c2aa976ceacc7eb3d11c6bd68f4515d21ec94b6c
Tessa Power <hello@tessapower.xyz>
2026-02-02 10:40:57 -0600

    Minor tidy up to reduce debugger warnings

--- platformer/scripts/checkpoint.gd
+++ platformer/scripts/checkpoint.gd
@@ -5,5 +5,5 @@ var last_location
 func _body_entered(body: Node2D) -> void:
 	if body is Player:
 		body.change_respawn_point(position)
-		
+
 		queue_free()
--- platformer/scripts/managers/music_manager.gd
+++ platformer/scripts/managers/music_manager.gd
@@ -20,26 +20,26 @@ func _ready() -> void:
 	"""
 	fade_music_in(t) - starts with 0
 	crossfade_sync_stream([0,1,2,3]) - 1=red, 2=green, 3=blue, call with [0,1] for red, etc.
-	var timer:SceneTreeTimer = get_tree().create_timer(10.0)  
-	timer.timeout.connect(func(): crossfade_sync_stream([0,1]))  
-	var timer2:SceneTreeTimer = get_tree().create_timer(20.0)  
+	var timer:SceneTreeTimer = get_tree().create_timer(10.0)
+	timer.timeout.connect(func(): crossfade_sync_stream([0,1]))
+	var timer2:SceneTreeTimer = get_tree().create_timer(20.0)
 	timer2.timeout.connect(func(): crossfade_sync_stream([0,2]))
-	var lpf_timer:SceneTreeTimer = get_tree().create_timer(10.0)  
+	var lpf_timer:SceneTreeTimer = get_tree().create_timer(10.0)
 	lpf_timer.timeout.connect(func(): low_pass_sweep(40, 5))
 	"""
 
 func fade_music_in(track: AudioStream, fade_time=track_fade_time) -> void:
 	current_music_player.stream = track
 	current_music_player.volume_db = mute_db
-	
+
 	# Set default playback to only first track if synchronized
 	if (track is AudioStreamSynchronized):
 		track.set_sync_stream_volume(0, default_music_db)
-		
+
 		# Mute other tracks
 		for i in range(1, track.stream_count):
 			track.set_sync_stream_volume(i, mute_db)
-	
+
 	current_music_player.play()
 
 	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
@@ -53,10 +53,10 @@ func fade_music_out(fade_time=track_fade_time) -> void:
 
 func crossfade_music_to(track: AudioStream) -> void:
 	fade_music_out() # Fade out first player
-	
+
 	# Switch current Player:
 	current_music_player = audio_stream_01 if current_music_player == audio_stream_02 else audio_stream_02
-	
+
 	fade_music_in(track) # Fade in second player
 
 
@@ -64,9 +64,9 @@ func crossfade_sync_stream(active_tracks: Array[int]):
 	# Fail silently if not playing synchronized tracks
 	if (current_music_player.stream is not AudioStreamSynchronized):
 		return
-		
+
 	var track : AudioStreamSynchronized = current_music_player.stream
-	
+
 	for i in range(track.stream_count):
 		if i not in active_tracks:
 			# Start mute transition
@@ -77,23 +77,23 @@ func crossfade_sync_stream(active_tracks: Array[int]):
 				mute_db,
 				sync_fade_time
 				)
-				
+
 		else:
 			# Start unmute transition
 			var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
 			tween.tween_method(
-				func(b): track.set_sync_stream_volume(i, b), 
+				func(b): track.set_sync_stream_volume(i, b),
 				track.get_sync_stream_volume(i),
 				default_music_db,
 				sync_fade_time
 				)
-		
+
 		track.set_sync_stream_volume(i, mute_db)
 
 
 func low_pass_sweep(end_freq, time, resonance=0.7):
 	var lpf = AudioServer.get_bus_effect(1, 0)
-	
+
 	# Update resonance
 	var reso_tween = create_tween().set_trans(Tween.TRANS_LINEAR)
 	reso_tween.tween_property(
@@ -102,8 +102,8 @@ func low_pass_sweep(end_freq, time, resonance=0.7):
 		lpf_resonance_time
 	)
 
-	var ease = Tween.EASE_IN if lpf.cutoff_hz < end_freq else Tween.EASE_OUT
-	var freq_tween = create_tween().set_ease(ease).set_trans(Tween.TRANS_EXPO)
+	var ease_type = Tween.EASE_IN if lpf.cutoff_hz < end_freq else Tween.EASE_OUT
+	var freq_tween = create_tween().set_ease(ease_type).set_trans(Tween.TRANS_EXPO)
 	freq_tween.tween_property(
 		lpf, "cutoff_hz",
 		end_freq,
