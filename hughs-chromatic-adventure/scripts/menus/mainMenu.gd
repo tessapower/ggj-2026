5ec6bdd35a9114c7f9b66578cc6f5fbbcdc872c9
Tessa Power <hello@tessapower.xyz>
2026-02-03 13:53:29 -0600

    Add basic buttons and transition manager to main menu

--- hughs-chromatic-adventure/scripts/menus/mainMenu.gd
+++ hughs-chromatic-adventure/scripts/menus/mainMenu.gd
@@ -1,7 +1,18 @@
-extends Node2D
+extends Control
+
+@export_file("*.tscn") var level_1: String
 
 var multistream_music = preload("res://assets/music/main_level/main_level_audio_stream.tres")
+
 
 func _ready() -> void:
 	# Start playing music
 	MusicManager.fade_music_in(multistream_music)
+
+
+func _on_play() -> void:
+	TransitionManager.fade_to_scene(level_1)
+
+
+func _on_exit() -> void:
+	get_tree().quit()
