b219d7d94de6209322982acaee7b10a81a23f5a4
Tessa Power <hello@tessapower.xyz>
2026-02-03 13:59:21 -0600

    Remove print statement in door script

--- hughs-chromatic-adventure/scripts/levelObjects/door.gd
+++ hughs-chromatic-adventure/scripts/levelObjects/door.gd
@@ -3,6 +3,5 @@ extends StaticBody2D
 @export_enum("Yellow", "Purple") var key_color = ""
 
 func _on_door_approach(body: Node2D) -> void:
-	print(body)
 	if body is Player:
 		print("check for key!")
